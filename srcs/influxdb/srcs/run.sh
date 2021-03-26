b2d494280a4c9a9507ae8646e69de7c1e492269a
Andreff16 <andr.b.2001@gmail.com>
2021-03-26 16:56:17 +0300

    influxdb init fixed

--- srcs/influxdb/srcs/run.sh
+++ srcs/influxdb/srcs/run.sh
@@ -1,7 +1,15 @@
 #!/bin/sh
 
 influxd run -pidfile /var/run/influxd.pid &
+
+sleep 1
 influx -execute "CREATE DATABASE grafana"
+while [ "$?" -eq 1 ]
+do
+	sleep 1
+	influx -execute "CREATE DATABASE grafana"
+done
+
 influx -execute "CREATE USER kfriese WITH PASSWORD 'kfriese'"
 influx -execute "GRANT ALL ON grafana TO kfriese"
 kill -SIGTERM $(cat /var/run/influxd.pid)
